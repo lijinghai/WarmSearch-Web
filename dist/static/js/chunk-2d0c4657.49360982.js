b406340a8d2ee5ecd5c710d4cd9eb291978453ef
Dorian15 <lijinghailjh@163.com>
2021-03-19 00:21:34 +0800

    npm i @vue/cli-service更新

--- dist/static/js/chunk-2d0c4657.49360982.js
+++
@@ -1,1 +0,0 @@
-(window["webpackJsonp"]=window["webpackJsonp"]||[]).push([["chunk-2d0c4657"],{"3b3e":function(e,n,t){"use strict";t.r(n);var a=function(){var e=this,n=e.$createElement,t=e._self._c||n;return t("h3",[e._v("文具")])},c=[],u={name:"Pen"},r=u,s=t("2877"),l=Object(s["a"])(r,a,c,!1,null,"63eaafd0",null);n["default"]=l.exports}}]);
