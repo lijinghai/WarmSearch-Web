74052a650c98add886eefd4fb632e9fe3c2b7823
Dorian1015 <lijinghailjh@163.com>
2021-05-24 11:33:15 +0800

    2021 5 24 个人中心

--- src/views/profile/index.vue
+++ src/views/profile/index.vue
@@ -10,12 +10,12 @@
         <el-col :span="18" :xs="24">
           <el-card>
             <el-tabs v-model="activeTab">
-              <el-tab-pane label="Activity" name="activity">
-                <activity />
-              </el-tab-pane>
-              <el-tab-pane label="Timeline" name="timeline">
-                <timeline />
-              </el-tab-pane>
+<!--              <el-tab-pane label="Activity" name="activity">-->
+<!--                <activity />-->
+<!--              </el-tab-pane>-->
+<!--              <el-tab-pane label="Timeline" name="timeline">-->
+<!--                <timeline />-->
+<!--              </el-tab-pane>-->
               <el-tab-pane label="Account" name="account">
                 <account :user="user" />
               </el-tab-pane>
@@ -37,11 +37,12 @@ import Account from './components/Account'
 
 export default {
   name: 'Profile',
-  components: { UserCard, Activity, Timeline, Account },
+  // components: { UserCard, Activity, Timeline, Account },
+  components: { Account , UserCard},
   data() {
     return {
       user: {},
-      activeTab: 'activity'
+      activeTab: 'account'
     }
   },
   computed: {
