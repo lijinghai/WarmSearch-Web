a3ff9d29e430850abe2728ecfd83378106be4919
lijinghai <lijinghailjh@163.com>
2022-10-02 13:15:32 +0800

    优化
    优化

--- src/views/dashboard/index.vue
+++ src/views/dashboard/index.vue
@@ -8,15 +8,15 @@
     <el-card shadow="always" body-style="padding: 5px 20px;">
       <el-tag type="success" style="width: 100%;">项目简介</el-tag>
       <p>这是我的校园失物招领网站的后台界面</p>
-      <p>校园失物招领网站，完成于本人大二期间（2021年 4月 24日），采用vue与springboot架构开发，前后端完全分离，json格式数据交互，全部由本人独立开发完成。将来将部署在本人的学生版阿里云centos7服务器上。</p>
+      <p>校园失物招领网站，采用vue与springboot架构开发，前后端完全分离，json格式数据交互，全部由本人独立开发完成。将来将部署在本人的学生版阿里云centos7服务器上。</p>
       <p>管理范围包括：PC端 + 微信小程序端</p>
       <p>该后台界面主要功能有：对PC端和微信小程序端数据进行管理；对认领人信息的管理；对PC端用户信息的审核</p>
       <el-tag type="success" style="width: 100%;">技术栈</el-tag>
       <p>前端后台管理系统由vue与elementUI搭建，路由使用vue-router，ajax请求均使用axios。</p>
       <p>mysql数据库，mybatis(通用Mapper)、实现了分页功能</p>
       <p>springBoot(spring、springMVC)</p>
-<!--      <p>springCloud(eureka、zuul、feign)搭建微服务工程</p>-->
-<!--      <p>nginx反向代理域名</p>-->
+      <!--      <p>springCloud(eureka、zuul、feign)搭建微服务工程</p>-->
+      <!--      <p>nginx反向代理域名</p>-->
       <p>采用jwt+rsa加密生成token，写入cookie的方式做无状态登录</p>
       <p>开发工具：webstorm、maven、idea、postman、swagger、git、navicat等</p>
     </el-card>
